c0c403b02c006adf40fd647467836ab5d073d72d
sstaples1069 <scottstarkstaples@gmail.com>
2023-11-12 17:58:27 -0800

    POST/GET fully functional

--- src/observations/observations.controller.js
+++ src/observations/observations.controller.js
@@ -1,5 +1,7 @@
-let nextId = 1;
-const observations = [];
+
+const service = require("./observations.service");
+const asyncErrorBoundary = require("../errors/asyncErrorBoundary");
+const { as } = require("../db/connection");
 
 const validSkyConditions = [100, 101, 102, 103, 104, 106, 108, 109]
 
@@ -36,14 +38,7 @@ function hasSkyCondition(req, res, next) {
 }
 
 async function create(req, res) {
-  const newObservation = req.body.data;
-
-  const now = new Date().toISOString();
-  newObservation.observation_id = nextId++;
-  newObservation.created_at = now;
-  newObservation.updated_at = now;
-
-  observations.push(newObservation);
+  const newObservation = await service.create(req.body.data);
 
   res.status(201).json({
     data: newObservation,
@@ -51,12 +46,13 @@ async function create(req, res) {
 }
 
 async function list(req, res) {
+    const data = await service.list();
     res.json({
-        data: observations
+        data
     })
 }
 
 module.exports = {
-  create: [hasData, hasLatitude, hasLongitude, hasSkyCondition, create],
-  list
+  create: [hasData, hasLatitude, hasLongitude, hasSkyCondition, asyncErrorBoundary(create)],
+  list: asyncErrorBoundary(list)
 };
