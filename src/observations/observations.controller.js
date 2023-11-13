88f3b2e684f2806021157e19681297c803cd55df
sstaples1069 <scottstarkstaples@gmail.com>
2023-11-12 17:25:42 -0800

    updated for GET requests

--- src/observations/observations.controller.js
+++ src/observations/observations.controller.js
@@ -1,4 +1,5 @@
 let nextId = 1;
+const observations = [];
 
 const validSkyConditions = [100, 101, 102, 103, 104, 106, 108, 109]
 
@@ -42,11 +43,20 @@ async function create(req, res) {
   newObservation.created_at = now;
   newObservation.updated_at = now;
 
+  observations.push(newObservation);
+
   res.status(201).json({
     data: newObservation,
   });
 }
 
+async function list(req, res) {
+    res.json({
+        data: observations
+    })
+}
+
 module.exports = {
   create: [hasData, hasLatitude, hasLongitude, hasSkyCondition, create],
+  list
 };
